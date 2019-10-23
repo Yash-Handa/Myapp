55acb60c2e34c8153934b69597376b80df0536ce
shubhampal98 <shubhampal1898@gmail.com>
2019-10-24 00:16:58 +0530

    added a footer component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Hello React</h1>
+      <div className="container">
+        <h1>Hello React</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
+          cupiditate corporis illo ipsam voluptatem, voluptas ipsa accusamus.
+          Porro ad aut perferendis aliquam! Laboriosam non molestias nostrum
+          officia aperiam tempore similique?
+        </p>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ myapp/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer class="footer mt-auto py-3">
+        <div class="container">
+          <p className="footer-text">&copy; 2019</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
