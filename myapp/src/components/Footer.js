d3d0ed6e39a62b690c5dcfacd085d3c1b65d1021
Shubham Pal <shubhampal1898@gmail.com>
2020-10-18 21:39:47 +0530

    fixed footer

--- myapp/src/components/Footer.js
+++ myapp/src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 export default function Footer() {
   return (
     <div>
-      <footer class="footer mt-auto py-3">
+      <footer class="footer mt-auto py-2">
         <div class="container">
-          <p className="footer-text">&copy; 2019</p>
+          <p className="footer-text">&copy; 2020 Made with love <FavoriteIcon /></p>
         </div>
       </footer>
     </div>
